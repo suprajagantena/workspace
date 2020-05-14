f4e0bf63c01dc9b36f4a9178afae1aaaff9e71cd
suprajagantena <suprajagantena@gmail.com>
2020-05-14 15:48:24 -0400

    Update workspace.tf

--- workspace.tf
+++ workspace.tf
@@ -3,13 +3,13 @@ resource "aws_vpc" "main" {
 }
 
 resource "aws_subnet" "private-a" {
-  vpc_id            = "aws_vpc.main.id"
+  vpc_id            = aws_vpc.main.id
   availability_zone = "us-east-1a"
   cidr_block        = "192.0.0.0/24"
 }
 
 resource "aws_subnet" "private-b" {
-  vpc_id            = "aws_vpc.main.id"
+  vpc_id            = aws_vpc.main.id
   availability_zone = "us-east-1b"
   cidr_block        = "192.0.1.0/24"
 }
@@ -19,7 +19,7 @@ resource "aws_directory_service_directory" "main" {
   password = "#S1ncerely"
   size     = "Small"
   vpc_settings {
-    vpc_id     = "aws_vpc.main.id"
+    vpc_id     = aws_vpc.main.id
     subnet_ids = ["${aws_subnet.private-a.id}", "${aws_subnet.private-b.id}"]
   }
 }
